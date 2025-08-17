2b32501e61c355e322f8e87ede5b78bbd0d00264
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2025-08-18 01:00:49 +0300

    First Commit

--- alx_backend_graphql/alx_backend_graphql/urls.py
+++ alx_backend_graphql/alx_backend_graphql/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for alx_backend_graphql_crm project.
+URL configuration for alx_backend_graphql project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
